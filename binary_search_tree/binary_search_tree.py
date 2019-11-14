00a0f509fdb1bebe1b2172ac5093d434f3175ba1
bkrant <gladiolusi70@gmail.com>
2019-11-14 00:52:45 -0500

    insert done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -6,14 +6,26 @@ from dll_stack import Stack
 
 class BinarySearchTree:
     def __init__(self, value):
-        self.value = value
+        self.value = value #this is the root node
         self.left = None
         self.right = None
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value == self.value:
+            return
+        elif value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
+            
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
