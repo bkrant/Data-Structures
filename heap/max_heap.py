4eed9398af53c9109a70609ce3df1f13bfa40e8a
bkrant <gladiolusi70@gmail.com>
2019-11-25 15:58:05 -0500

    heapify helper function

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -4,11 +4,13 @@ class Heap:
 
     def insert(self, value):
         insert_list = [None] + self.storage + [value]
-        self._heapify(insert_list)
+        self.storage = self._heapify(insert_list)
 
     def delete(self):
         delete_list = [None] + [self.storage[-1]] + self.storage[1:-1]
-        self._heapify(delete_list)
+        removed_element = self.get_max()
+        self.storage = self._heapify(delete_list)
+        return removed_element
 
     def get_max(self):
         return self.storage[0]
@@ -26,7 +28,7 @@ class Heap:
         while True:
             swaps = 0
             for i in range((len(list_to_heapify)-1)//2 ,0, -1):
-                if 2*i+1 <= len(list_to_heapify)-1:
+                try: #if 2*i+1 <= len(list_to_heapify)-1:
                     if list_to_heapify[i] < list_to_heapify[2*i] or list_to_heapify[i] < list_to_heapify[2*i+1]:
                         max_child = max(list_to_heapify[2*i], list_to_heapify[2*i+1])
                         if list_to_heapify[2*i] == max_child:
@@ -36,7 +38,7 @@ class Heap:
                         list_to_heapify[i], list_to_heapify[max_child_index] = max_child, list_to_heapify[i]
                         swaps += 1
 
-                else:
+                except:
                     if list_to_heapify[i] < list_to_heapify[2*i]:
                         list_to_heapify[i], list_to_heapify[2*i] = list_to_heapify[2*i], list_to_heapify[i]
                         swaps += 1
@@ -44,4 +46,4 @@ class Heap:
             if swaps == 0:
                 break
 
-        self.storage = list_to_heapify[1:]
+        return(list_to_heapify[1:])
